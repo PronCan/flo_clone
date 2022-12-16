b90860c7097899fbde5114741579ddd5357b52e5
PronCan <twojini98@gmail.com>
2022-12-16 11:17:14 +0900

    221215

--- flo_clone/resource/js/main.js
+++ flo_clone/resource/js/main.js
@@ -1,1 +1,11 @@
-$('.content_today_music');
+
+$(window).on('load', function() {
+    $('header div div nav p a').removeClass();
+    // load swipe data
+    $.getJSON('./data/main_data.json', function(data) {
+        console.log(data);
+    });
+    
+});
+
+// main swiper
